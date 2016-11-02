16ea1b771e20e2a09469f488dde0b0bda892f202
JackCheng520 <302517623@qq.com>
2016-11-02 17:35:53 +0800

    UGUI点击穿透事件

---
+++ UI事件穿透/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// ================================
+//* 功能描述：Test  
+//* 创 建 者：chenghaixiao
+//* 创建日期：2016/9/7 9:29:16
+// ================================
+namespace Assets.JackCheng.UI事件穿透
+{
+    public class Test : MonoBehaviour
+    {
+        void OnEnable()
+        {
+            UGUIEventTest.onClick += OnClick;
+        }
+
+        void OnDisable()
+        {
+            UGUIEventTest.onClick -= OnClick;
+        }
+
+        private Button btn;
+        void Start() {
+            btn = gameObject.GetComponent<Button>();
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(OnClick);
+            
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log(gameObject.name + "Clicked --");
+        }
+
+        private void OnClick()
+        {
+            Debug.Log(gameObject.name + "Clicked --");
+        }
+    }
+}
